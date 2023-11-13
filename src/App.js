a26bcb24bfd18ca0bd70bbbed8385c706c65600d
AmiraFareh <amirafareh137@gmail.com>
2023-11-14 11:15:21 +1300

    Added GitHub repo URL and portfolio URL.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ export default function App() {
   return (
     <div className="App">
       Weather App
+      <footer>
+        This project was created by <a href="https://amira-fareh-portfolio.netlify.app" target='_blank' rel="noreferrer">Amira fareh</a> and is 
+      <a href='https://github.com/AmiraFareh/react-weather-app' target='_blank' rel="noreferrer"> open-sourced on GitHub. </a>
+      </footer>
     </div>
   );
 }
