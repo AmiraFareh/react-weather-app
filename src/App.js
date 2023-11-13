11588fce41278c79880d57abf21643edceaf3264
AmiraFareh <amirafareh137@gmail.com>
2023-11-14 11:54:26 +1300

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+    <div className="container"> 
       Weather App
+      
       <footer>
         This project was created by <a href="https://amira-fareh-portfolio.netlify.app" target='_blank' rel="noreferrer">Amira fareh</a> and is 
       <a href='https://github.com/AmiraFareh/react-weather-app' target='_blank' rel="noreferrer"> open-sourced on GitHub. </a>
       </footer>
+      </div>
     </div>
   );
 }
