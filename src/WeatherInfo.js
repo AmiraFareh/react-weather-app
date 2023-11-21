ff5d2cc404c06d2224955e9d301f59174b6c2f77
AmiraFareh <amirafareh137@gmail.com>
2023-11-21 16:50:40 +1300

    fixed styling error of units.

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -3,71 +3,60 @@ import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
 import WeatherTemperature from "./WeatherTemperature";
 
-export default function WeatherInfo(props){
-    return(
-<div className="WeatherInfo">
-<h1 className="mb-3 ">{props.data.city}, {props.data.country}</h1>
-            <div className="wrap">
-                <h2>Current weather </h2>
-                <h3 className="mb-5">
-                <FormattedDate date = {props.data.date}/>
-                </h3>
-                <div className="grid mb-5">
-                    <div className="row">
-                    <div className="col-6">
-                    <div className="clearfix">
-                    <div className="float-left">
-                        </div>   
-                    <WeatherIcon code = {props.data.icon}/>
-                    <WeatherTemperature celsius = {props.data.temperature}/>
-                   
-                    </div>
-                    </div>
-                   
-                    <div className="col-6 weather-description text-capitalize">
-                        {props.data.description}
-                        <div className="feels-like ">
-                            Feels like {props.data.feelsLike}<span className="feels-like-unit">°C</span>
-                        </div>
-                        
-                    </div>
-                    </div>
-                   
-                </div>
-                
-                <div className="weather-desc-grid">
-                <div className="row">
-                    <div className="col wind-label">
-                    Wind
-                    <div className="wind-speed">
-                    {props.data.wind} km/h
-                    </div>
-                    </div>
-                    <div className="col humidity-label ">
-                    Humidity
-                    <div className="humidity">
-                    {props.data.humidity}%
-                    </div>
-                    </div>
-                    <div className="col visibility-label">
-                    Visibility
-                    <div className="visibility">
-                    {props.data.visibility} km
-                    </div>
-                    </div>
-                    <div className="col pressure-label">
-                    Pressure
-                    <div className="pressure">
-                    {props.data.pressure} mb
-                    </div>
-                    </div>
-                   
-                    
-
-                </div>
-                </div>
+export default function WeatherInfo(props) {
+  return (
+    <div className="WeatherInfo">
+      <h1 className="mb-3 ">
+        {props.data.city}, {props.data.country}
+      </h1>
+      <div className="wrap">
+        <h2>Current weather </h2>
+        <h3 className="mb-5">
+          {" "}
+          Updated:
+          <FormattedDate date={props.data.date} />
+        </h3>
+        <div className="grid mb-5">
+          <div className="row">
+            <div className="col-6">
+              <div className="clearfix">
+                <div className="float-left"></div>
+                <WeatherIcon code={props.data.icon} />
+                <WeatherTemperature celsius={props.data.temperature} />
+              </div>
             </div>
-</div>
-    )
-    
+
+            <div className="col-6 weather-description text-capitalize">
+              {props.data.description}
+              <div className="feels-like ">
+                Feels like {props.data.feelsLike}
+                <span className="feels-like-unit">°C</span>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="weather-desc-grid">
+          <div className="row">
+            <div className="col wind-label">
+              Wind
+              <div className="wind-speed">{props.data.wind} km/h</div>
+            </div>
+            <div className="col humidity-label ">
+              Humidity
+              <div className="humidity">{props.data.humidity}%</div>
+            </div>
+            <div className="col visibility-label">
+              Visibility
+              <div className="visibility">{props.data.visibility} km</div>
+            </div>
+            <div className="col pressure-label">
+              Pressure
+              <div className="pressure">{props.data.pressure} mb</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,45 +1,46 @@
+import React, { useState } from "react";
 
-import React, {useState} from "react";
+export default function WeatherTemperature(props) {
+  const [unit, setUnit] = useState("celsius");
 
-export default function WeatherTemperature(props){
-    const[unit, setUnit] = useState("celsius");
+  function showFahrenheit(event) {
+    event.preventDefault();
+    setUnit("fahrenheit");
+  }
 
-    function showFahrenheit(event){
-        event.preventDefault();
-        setUnit("fahrenheit")
-    }
+  function showCelsius(event) {
+    event.preventDefault();
+    setUnit("celsius");
+  }
 
-    function showCelsius(event){
-        event.preventDefault();
-        setUnit("celsius")
-    }
-
-    
-    
-    if(unit === "celsius"){
-        return(
-            <span className="WeatherTemperature">
-                 <span className="float-left">
-                        <span className="current-temp">{props.celsius}</span>
-                        <span className="unit">°C | <a href="/" onClick = {showFahrenheit}>°F </a></span>
-                       
-                        </span>
-            </span>
-        )
-    }
-    else{
-
-        let fahrenheit = (props.celsius *9) /5 +32; 
-        return(
-            <span className="WeatherTemperature">
-                 <span className="float-left">
-                        <span className="current-temp">{fahrenheit}</span>
-                        <span className="unit"><a href="/" onClick={showCelsius}>°C </a></span>| °F
-                       
-                        </span>
-            </span>
-        )
-    }
-    
-   
+  if (unit === "celsius") {
+    return (
+      <span className="WeatherTemperature">
+        <span className="float-left">
+          <span className="current-temp">{props.celsius}</span>
+          <span className="unit">
+            °C |{" "}
+            <a href="/" onClick={showFahrenheit}>
+              °F{" "}
+            </a>
+          </span>
+        </span>
+      </span>
+    );
+  } else {
+    let fahrenheit = Math.round((props.celsius * 9) / 5 + 32);
+    return (
+      <span className="WeatherTemperature">
+        <span className="float-left">
+          <span className="current-temp">{fahrenheit}</span>
+          <span className="unit">
+            <a href="/" onClick={showCelsius}>
+              °C{" "}
+            </a>
+            | °F{" "}
+          </span>
+        </span>
+      </span>
+    );
+  }
 }
